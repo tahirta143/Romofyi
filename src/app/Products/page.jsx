643f75c103f105d86f937a8318490e76858380a3
Muhammad Tahir <mtahirmusman2@gmail.com>
2025-10-01 19:37:13 +0500

    updated

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-function Product() {
+function Products() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Hero from "./hero/page";
-import Product from "./products/page";
+
 import Fashion from "./fashion/page";
 import News from "./news/page";
+import Products from "./products/page";
 
 export default function Page() {
   return (
     <div>
       <Hero />
-      <Product />
+      <Products />
       <Fashion />
       <News />
     </div>
