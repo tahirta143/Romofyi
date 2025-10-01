cebead136f00c0e33ed1db54226d19f311877de9
Muhammad Tahir <mtahirmusman2@gmail.com>
2025-10-01 18:01:11 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Hero from "./hero";
 import Products from "./Products/page";
-import Fashion from "./fashion/page";
-import News from "./news/page";
-
+import Fashion from "./Fashion/page";
+import News from "./News/page";
 export default function Page() {
   return (
     <div>
