a80f0c87d86e92c3df306086fcc77a90f2cce905
Muhammad Tahir <mtahirmusman2@gmail.com>
2025-10-01 17:28:01 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "./hero";
-import Products from "./[Products]/page";
+import Products from "./Products/page";
 import Fashion from "./Fashion/page";
 import News from "./News/page";
 
